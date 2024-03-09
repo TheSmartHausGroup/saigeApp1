57548076e8ab17fa46100e44083f83c1aa9ae2d1
Siniscalchi13 <151758328+Siniscalchi13@users.noreply.github.com>
2024-03-09 09:13:39 -0500

    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,11 +3,30 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:28f83a68-6f8e-4c51-bd89-dc39a1815ba1",
+    "aws_cloud_logic_custom": [
+        {
+            "name": "saigeAppAPI",
+            "endpoint": "https://m2fsghsrqc.execute-api.us-east-1.amazonaws.com/saigedev",
+            "region": "us-east-1"
+        }
+    ],
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_pCsSgxvxt",
-    "aws_user_pools_web_client_id": "6reftvmga3aukjs9sjicocreef",
-    "oauth": {},
+    "aws_user_pools_id": "us-east-1_qwtzwRU73",
+    "aws_user_pools_web_client_id": "7tpcgabmushlhs3a0udtv307h8",
+    "oauth": {
+        "domain": "saigeapp1ea3aa9b1-ea3aa9b1-saigedev.auth.us-east-1.amazoncognito.com",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "https://example.com/auth/",
+        "redirectSignOut": "https://example.com/auth/",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
     "aws_cognito_username_attributes": [],
     "aws_cognito_social_providers": [],
     "aws_cognito_signup_attributes": [
@@ -23,13 +42,6 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ],
-    "aws_cloud_logic_custom": [
-        {
-            "name": "saigeAppAPI",
-            "endpoint": "https://ynezysp2m6.execute-api.us-east-1.amazonaws.com/dev",
-            "region": "us-east-1"
-        }
     ]
 };
 
