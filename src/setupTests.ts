96c8f812fa77995ffc254a72ff8eac208c97573b
Siniscalchi13 <151758328+Siniscalchi13@users.noreply.github.com>
2024-03-25 14:10:03 -0400

    test update

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,11 +2,24 @@
 import '@testing-library/jest-dom';
 
 class MockAudioContext {
-  createOscillator() {
-    // Mock implementation or empty function
+    createOscillator() {
+      // Mock implementation or empty function
+    }
+    createMediaStreamSource() {
+      // Mock implementation or return a dummy object as needed
+      return {};
+    }
+    createAnalyser() {
+      // Mock implementation for createAnalyser
+      return {
+        // Add mock implementations for any methods you use on the analyser
+        fftSize: 2048,
+        getByteTimeDomainData: jest.fn(),
+        // Continue mocking as necessary
+      };
+    }
   }
-  // Add other methods you might use
-}
+  
 
 // Mock for AudioContext
 global.AudioContext = MockAudioContext as any;
@@ -15,11 +28,12 @@ global.AudioContext = MockAudioContext as any;
 const mockMediaRecorder = jest.fn().mockImplementation(() => ({
   start: jest.fn(),
   stop: jest.fn(),
+  onDataAvailable: jest.fn(),
+  onstop: jest.fn(),
   // Mock other methods as needed
 }));
 
 // Directly assign isTypeSupported to the mock function object
-// TypeScript recognizes this addition as a property on the function object itself
 (mockMediaRecorder as any).isTypeSupported = jest.fn(() => true);
 
 // Assign the mock to global.MediaRecorder
